1fca691faa0a6f11b66ee13218b61017efa48aab
Jeramy Dichiera <jeramy.dichiera@gmail.com>
2022-05-15 14:29:33 -0400

    Add mock products

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core'
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  products: any[] = [{
+    "productId": 2,
+    "productName": "Garden Cart",
+    "productCode": "CODE-123",
+    "releaseDate": "March 18, 2022",
+    "description": "A big garden cart",
+    "price": 39.99,
+    "starRating": 4.2,
+    "imageUrl": "assets/images/garden_cart.png"
+  },
+  {
+    "productId": 3,
+    "productName": "Hammer",
+    "productCode": "CODE-456",
+    "releaseDate": "March 21, 2022",
+    "description": "A big hammer",
+    "price": 8.99,
+    "starRating": 4.8,
+    "imageUrl": "assets/images/hammer.png"
+  }];
 }
